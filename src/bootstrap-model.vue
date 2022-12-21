d7b51f3166b5f346984c3e78473029c9d0d23b90
DivakarSS <69146417+DivakarSS@users.noreply.github.com>
2022-12-21 16:34:42 +0530

    Update bootstrap-model.vue

--- src/bootstrap-model.vue
+++ src/bootstrap-model.vue
@@ -1,20 +1,20 @@
 
 <template>
-  <b-button @click="modal = !modal">
+  <BButton @click="modal = !modal">
   Toggle modal
-</b-button>
+  </BButton>
 
-<b-modal v-model="modal">
+<BModal v-model="modal">
   Foo bar
-</b-modal>
+</BModal>
 </template>
 
 <script setup lang="ts">
 import {ref} from 'vue'
-import { createApp } from 'vue'
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-createApp({}).use(BootstrapVue3);
+// import { createApp } from 'vue'
+// import BootstrapVue3 from 'bootstrap-vue-3'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+// createApp({}).use(BootstrapVue3);
 const modal = ref(false)
 </script>
