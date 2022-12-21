4fd1de91f1c016333c24ddd1ab3aeb86ebf18fc8
DivakarSS <69146417+DivakarSS@users.noreply.github.com>
2022-12-21 16:48:58 +0530

    Add files via upload

---
+++ src/components/bootstrap-model.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <BButton @click="modal = !modal">
+  Toggle modal
+  </BButton>
+
+<BModal v-model="modal">
+  Foo bar
+</BModal>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+// import { createApp } from 'vue'
+// import BootstrapVue3 from 'bootstrap-vue-3'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+// createApp({}).use(BootstrapVue3);
+const modal = ref(false)
+</script>
